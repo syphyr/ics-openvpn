952feb47534cf2847484a9e4173e0ad9e7efd835
syphyr <syphyr@gmail.com>
2024-09-15 20:44:02 +0200

    Update to NDK 27b

--- main/build.gradle.kts
+++ main/build.gradle.kts
@@ -21,7 +21,7 @@ android {
     //compileSdkPreview = "UpsideDownCake"
 
     // Also update runcoverity.sh
-    ndkVersion = "28.0.12674087-rc1"
+    ndkVersion = "27.1.12297006"
 
 
     defaultConfig {
